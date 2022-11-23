83499806b5a1ae7ad8180ca903a7c9f59e7477af
List Kelvin <phamm5687@gmail.com>
2022-11-23 22:02:41 +0700

    Update  wishlist and cartPage

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -1,7 +1,29 @@
+const Array01 = [
+  {
+    name: "minh",
+    age: 18,
+  },
+  {
+    name: "Kiet",
+    age: 22,
+  },
+  {
+    name: "Long",
+    age: 30,
+  },
+];
+
 const CartPage = () => {
   return (
     //mọi code phải nằm trong thẻ này (<> </>)
     <>
+      {Array01.map(({ name, age }, id) => (
+        <div key={id}>
+          <h1>{name}</h1>
+          <span> {age}</span>
+        </div>
+      ))}
+
       <h1> this is Cart page</h1>
     </>
   );
