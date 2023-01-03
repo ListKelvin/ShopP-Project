3cd8e0b572718f4df7c31727bca9249c0804f821
List Kelvin <phamm5687@gmail.com>
2023-01-03 23:04:07 +0700

    Update UI Tabs card

--- src/pages/UserPurchase/index.js
+++ src/pages/UserPurchase/index.js
@@ -1,43 +1,66 @@
-import React, {useState} from 'react';
-import { EvaluationDiv, EvaluationsContainer, EvaluationTitle, } from "../UserEvaluation/style";
-import {FilterContainer, } from "../UserVouchers/Components/styleComponents";
-import {TitlePurchase, TitlePurchaseAfter, TitlePurchaseDiv} from "./style";
-import ProductPurchase from "./Components/ProductPurchase";
+import React, { useState } from "react";
+import {
+  EvaluationDiv,
+  EvaluationsContainer,
+  EvaluationTitle,
+} from "../UserEvaluation/style";
+import { FilterContainer } from "../UserVouchers/Components/styleComponents";
+import { TitlePurchase, TitlePurchaseAfter, TitlePurchaseDiv } from "./style";
+import TabGroup from "../../Component/Tabs";
+
 const Categories = [
-  {id: 1, label: "All"},
-  {id: 2, label: "Checking"},
-  {id: 3, label: "Confirmed"},
-  {id: 4, label: "Packaging"},
-  {id: 5, label: "Delivering"},
-  {id: 6, label: "Delivered"},
-  {id: 7, label: "Cancelled"},
-  {id: 7, label: "Returned"},
-]
+  { id: 0, label: "All" },
+  { id: 1, label: "Checking" },
+  { id: 2, label: "Confirmed" },
+  { id: 3, label: "Packaging" },
+  { id: 4, label: "Delivering" },
+  { id: 5, label: "Delivered" },
+  { id: 6, label: "Cancelled" },
+  { id: 7, label: "Returned" },
+];
 
 const UserPurchase = () => {
   const [isActive, setActive] = useState(false);
 
   const toggleClass = () => {
     setActive(!isActive);
-  }
+  };
+
   return (
-  <EvaluationDiv>
-    <EvaluationsContainer>
-      <EvaluationTitle>My Purchase</EvaluationTitle> 
-      <FilterContainer>
-        <TitlePurchaseDiv>
-          {Categories.map (({id, label}) => {return (
-            <TitlePurchase 
-            key = {id}
-            className={isActive ? 'Title' : null}
-            onClick={toggleClass}
-            >{label}</TitlePurchase>
-            );})}
-        </TitlePurchaseDiv>
-      </FilterContainer>
-      <ProductPurchase/>
-    </EvaluationsContainer>
-  </EvaluationDiv>)
+    <EvaluationDiv>
+      <EvaluationsContainer>
+        <EvaluationTitle>My Purchase</EvaluationTitle>
+        <FilterContainer>
+          <TitlePurchaseDiv>
+            {Categories.map(({ id, label }) => {
+              return (
+                <TitlePurchase
+                  key={id}
+                  className={isActive ? "Title" : null}
+                  onClick={toggleClass}
+                >
+                  {label}
+                </TitlePurchase>
+              );
+            })}
+          </TitlePurchaseDiv>
+        </FilterContainer>
+        <TabGroup />
+      </EvaluationsContainer>
+    </EvaluationDiv>
+  );
 };
 
+// <Tabs>
+//   <div label="Gator">
+//     See ya later, <em>Alligator</em>!
+//   </div>
+//   <div label="Croc">
+//     After 'while, <em>Crocodile</em>!
+//   </div>
+//   <div label="Sarcosuchus">
+//     Nothing to see here, this tab is <em>extinct</em>!
+//   </div>
+// </Tabs>
+// <ProductPurchase />
 export default UserPurchase;
